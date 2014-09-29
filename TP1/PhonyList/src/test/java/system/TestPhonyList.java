27e5ebe94ac487abdab63405c6a495004278e65e
hacault.robin <hacault.robin@free.fr>
2014-09-29 15:08:12 +0200

    [TP1] - Test isEmpty()

--- TP1/PhonyList/src/test/java/system/TestPhonyList.java
+++ TP1/PhonyList/src/test/java/system/TestPhonyList.java
@@ -2,10 +2,17 @@ package system;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestPhonyList {
 
-	
+	@Test
+    public void testIsEmpty () {
+        PhonyList<Integer> list = new PhonyList<Integer>();
+        assertTrue(list.isEmpty());
+        list.add(1);
+        assertFalse(list.isEmpty());
+    }
 
 }
