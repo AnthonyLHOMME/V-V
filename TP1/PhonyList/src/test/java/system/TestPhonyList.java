dc8df48bb686420cc41d1940006dd712d649bec2
Anthony LHOMME <lhomme.anthony@gmail.com>
2014-09-29 15:08:42 +0200

    [TP1] test case - size

--- TP1/PhonyList/src/test/java/system/TestPhonyList.java
+++ TP1/PhonyList/src/test/java/system/TestPhonyList.java
@@ -1,11 +1,18 @@
 package system;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestPhonyList {
 
-	
+	@Test
+    public void testSize() {
+        PhonyList<Integer> list = new PhonyList<>();
+        assertEquals(list.size(),0);
+        list.add(1);
+        assertEquals(list.size(), 1);
+    }
 
 }
