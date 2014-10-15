7d743f2207e46dc05cfe8c922152a42d6c28a376
Anthony LHOMME <lhomme.anthony@gmail.com>
2014-10-15 12:09:21 +0200

    [TP4] Implementation LazyBiMap

--- TP4/map/src/main/java/vv/tp3/LazyBiMap.java
+++ TP4/map/src/main/java/vv/tp3/LazyBiMap.java
@@ -6,6 +6,31 @@ import vv.tp3.factory.Factory;
 
 public class LazyBiMap<K,V> extends BiMap<K,V> {
 
+    private Factory<K, V> keyFactory;
+    private Factory<V, K> valueFactory;
+
     public LazyBiMap(Factory<K, V> keyFactory, Factory<V, K> valueFactory) {
-   }
+        this.keyFactory = keyFactory;
+        this.valueFactory = valueFactory;
+    }
+
+    public V get(Object key) {
+        if (containsKey(key)) {
+            return super.get(key);
+        } else {
+            V value = valueFactory.transform((K) key);
+            return put((K) key, value);
+        }
+    }
+
+    public K getByValue(Object value) {
+        if (containsValue(value)) {
+            return  super.getByValue(value);
+        } else {
+            K key = keyFactory.transform((V) value);
+            put(key, (V) value);
+            return key;
+        }
+    }
+
 }
