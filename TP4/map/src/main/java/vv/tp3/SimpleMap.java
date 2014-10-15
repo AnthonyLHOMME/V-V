86ef981ac8d4f5a47f20f0355a4e9fe71966c966
Anthony LHOMME <lhomme.anthony@gmail.com>
2014-10-15 10:59:16 +0200

    [TP4] Implementation SimpleMap

--- TP4/map/src/main/java/vv/tp3/SimpleMap.java
+++ TP4/map/src/main/java/vv/tp3/SimpleMap.java
@@ -1,67 +1,88 @@
 package vv.tp3;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.*;
 
 public class SimpleMap<K,V> implements Map<K,V> {
 
+    private List<K> keys;
+    private List<V> values;
+
+    public SimpleMap() {
+        keys = new ArrayList<K>();
+        values = new ArrayList<V>();
+    }
 
     @Override
     public int size() {
-        return 0;
+        return keys.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return keys.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        return keys.contains(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        return values.contains(value);
     }
 
     @Override
     public V get(Object key) {
-        return null;
+        int index = keys.indexOf(key);
+        return values.get(index);
     }
 
     @Override
     public V put(K key, V value) {
-        return null;
+        keys.add(key);
+        values.add(value);
+        return value;
     }
 
     @Override
     public V remove(Object key) {
-        return null;
+        int index = keys.indexOf(key);
+
+        keys.remove(index);
+        return values.remove(index);
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
+        for(K key : m.keySet()) {
+            V value = m.get(key);
 
+            keys.add(key);
+            values.add(value);
+        }
     }
 
     @Override
     public void clear() {
-
+        keys.clear();
+        values.clear();
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        return new HashSet<K>(keys);
     }
 
     @Override
     public Collection<V> values() {
-        return null;
+        return values;
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        throw new NotImplementedException();
     }
 }
