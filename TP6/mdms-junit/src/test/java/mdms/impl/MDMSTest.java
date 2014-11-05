8250eaea2652d48649089f0482134d2bdeac7991
hacaultrobin <hacault.robin@free.fr>
2014-11-05 11:42:24 +0100

    [TP6] - Init project + Premier test Selenium

---
+++ TP6/mdms-junit/src/test/java/mdms/impl/MDMSTest.java
@@ -0,0 +1,44 @@
+package mdms.impl;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import mdms.AbstractMDMSTest;
+
+public class MDMSTest extends AbstractMDMSTest {
+
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testMDMS() throws Exception {
+    driver.get(baseUrl + "/");
+    try {
+      assertEquals("MdMS - Diversify", driver.getTitle());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+}
