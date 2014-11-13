daa766a7f4af0deeba4219df11e70193bc684998
Anthony LHOMME <lhomme.anthony@gmail.com>
2014-11-13 16:22:34 +0100

    [TP7] init

---
+++ TP7/spoon/src/main/java/vv/spoon/processor/CountExecutedTestProcessor.java
@@ -0,0 +1,15 @@
+package vv.spoon.processor;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtMethod;
+
+/**
+ * Created by anthony on 13/11/14.
+ */
+public class CountExecutedTestProcessor extends AbstractProcessor<CtMethod> {
+
+    @Override
+    public void process(CtMethod e) {
+
+    }
+}
